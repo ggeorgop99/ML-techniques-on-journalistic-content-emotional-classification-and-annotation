d0ecba78358968fbddcb16a76b96c1e3872a0322
Georgios Georgopoulos <75550770+ggeorgop99@users.noreply.github.com>
2024-08-22 21:03:12 +0300

    first commit
    transferred the 'till now worked on "mysentistength" files to this official thesis repo for the final sprint of the thesis

---
+++ utils.py
@@ -0,0 +1,9 @@
+import os
+
+def generate_unique_filename(directory, file_name, mode, extension):
+    counter = 1
+    file_path = os.path.join(directory, f"{file_name}_{mode}.{extension}")
+    while os.path.exists(file_path):
+        file_path = os.path.join(directory, f"{file_name}_{counter}_{mode}.{extension}")
+        counter += 1
+    return file_path
